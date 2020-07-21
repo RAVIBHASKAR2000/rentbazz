b723cc774f070728557bd8c1559b8fb94888b56a
Ravi Bhaskar <bhaskarravi2000@gmail.com>
2020-07-21 09:41:16 +0530

    search button

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import { Navbar,Nav,NavItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 class Header extends Component{
@@ -13,7 +14,9 @@ class Header extends Component{
                     <NavItem> 
                         <form class="form-inline my-2 my-lg-0">
                             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <Link to='\home'>
                             <button class="navbtn btn btn-outline-success my-2 my-sm-0" type="submit">  <span className="fa fa-search fa-lg"></span> Search</button>
+                            </Link>
                         </form>    
                     </NavItem>
 
